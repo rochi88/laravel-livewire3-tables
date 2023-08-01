a7702b0e6460e9c613aa09f84de981e9d8868c08
rochi88 <raisul.me@gmail.com>
2023-08-01 15:33:38 +0600

    .

---
+++ resources/views/components/checkbox-all.blade.php
@@ -0,0 +1,3 @@
+<th class="flex items-center" style="width: 1%;">
+    <input type="checkbox" wire:model="checkbox_all">
+</th>
---
+++ resources/views/components/checkbox-row.blade.php
@@ -0,0 +1,3 @@
+<td class="flex items-center" style="width: 1%;">
+    <input type="checkbox" wire:model="checkbox_values" value="{{ $model->{$checkbox_attribute} }}">
+</td>
